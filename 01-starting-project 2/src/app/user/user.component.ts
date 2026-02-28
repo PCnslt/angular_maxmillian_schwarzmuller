11bbf77fccbfb952de3da19469f3f14c07a4c838
PCnslt <info@pcnslt.com>
2026-02-28 14:11:03 -0500

    35 Working with outputs and emitting data.

--- 01-starting-project 2/src/app/user/user.component.ts
+++ 01-starting-project 2/src/app/user/user.component.ts
@@ -1,27 +1,30 @@
-import { Component, computed, signal } from "@angular/core";
-import { DUMMY_USERS } from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, Input, Output, EventEmitter, computed, input} from '@angular/core'
 @Component({
-  selector: "app-user",
-  standalone: true,
-  imports: [],
-  templateUrl: "./user.component.html",
-  styleUrl: "./user.component.css",
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl:'./user.component.css'
 })
-export class UserComponent {
-  // selectedUser = DUMMY_USERS[randomIndex];
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => "assets/users/" + this.selectedUser().avatar);
+export class UserComponent{
 
-  // get imagePath(){
-  //   return 'assets/users/'+this.selectedUser.avatar ;
-  // }
+    @Input() id!: string;
+    @Input() avatar!: string;
+    @Input() name!: string;
+    @Output() select = new EventEmitter();
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    // this.selectedUser = DUMMY_USERS[randomIndex];
-    // console.log("Button was clicked.");
-  }
+    // avatar = input.required<string>();
+    // name = input.required<string>();
+
+    // imagePath = computed(()->{
+    //     return 'assets/users/'+this.avatar;
+    // });
+
+    get imagePath(){
+        return 'assets/users/'+this.avatar;
+    }
+
+    onSelectUser(id: string){
+        this.select.emit(this.id);
+    }
+
 }
